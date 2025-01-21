f2140f3b7302301e26d2133798af3051a2d5d393
DESKTOP-ATKD7PL\ivanh <ivanholovchak05@gmail.com>
2025-01-22 01:37:09 +0200

    Rewrite character movement and looking logic

--- Assets/Engine/Player.cs
+++ Assets/Engine/Player.cs
@@ -7,11 +7,9 @@ namespace Engine
     public class Player : MonoBehaviour
     {
         [SerializeField] private float mouseSensitivity = 1f;
-        [SerializeField] private float walkSpeed;
+        [SerializeField] private float moveSpeed;
 
-        private Vector2 _mouseInput;
-        private Vector3 _moveDirection;
-        private Vector3 _movement;
+        private float _characterVelocityY;
 
         private CharacterController _characterController;
 
@@ -22,25 +20,36 @@ namespace Engine
 
         private void Update()
         {
-            RotationHandle();
-            MoveHandle();
+            HandleCharacterLook();
+            HandleCharacterMovement();
 
             CursorHandle();
         }
 
-        private void RotationHandle()
+        private void HandleCharacterLook()
         {
-            _mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + Vector3.up * _mouseInput.x);         
+            float lookX = Input.GetAxisRaw("Mouse X");
+            transform.Rotate(new Vector3(0f, lookX * mouseSensitivity, 0f), Space.Self);
         }
 
-        private void MoveHandle()
+        private void HandleCharacterMovement()
         {
-            _moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            Vector3 characterVelocity = (transform.right * moveDirection.x * moveSpeed + transform.forward * moveDirection.y * moveSpeed);
 
-            _movement = ((transform.right * _moveDirection.x) + (transform.forward * _moveDirection.z)).normalized * walkSpeed;
-
-            _characterController.Move(_movement * Time.deltaTime);
+            if (_characterController.isGrounded)
+            {
+                _characterVelocityY = 0f;
+                //if (Input.GetKeyDown(KeyCode.Space))
+                //{
+                //    float jumpSpeed = 30f;
+                //    _characterVelocityY = jumpSpeed;
+                //}
+            }
+            float gravityDownForce = -110f;
+            _characterVelocityY += gravityDownForce * Time.deltaTime;
+            characterVelocity.y = _characterVelocityY;
+            _characterController.Move(characterVelocity * Time.deltaTime);
         }
 
         private void CursorHandle()
